72fb8660b54caa4a20a47b592d2c10a5f7448a65
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-16 14:57:41 +0700

    Add conditions to if and elsif to check for i_lenght and a == b

--- script.rb
+++ script.rb
@@ -1,9 +1,9 @@
 num_array = [4,3,78,2,0,2]
 
-  def bubble_sort(num_array)  
-    i_length = num_array.length
-    i = 0 
-  while i < i_length do
+def bubble_sort(num_array)
+  i_length = num_array.length
+  i = 0
+  while i <= i_length-1 do
     if num_array.sorted?
       p num_array
       break
@@ -11,27 +11,27 @@ num_array = [4,3,78,2,0,2]
       i = 0
     end
       num_array.each_cons(2).map { |pair| 
-      p "i = #{i}" 
-      indexA = i
-      indexB = i+1
-      p pair = [num_array[indexA], num_array[indexB]]
-      a = pair[0]
-      b = pair[1]     
-   if a > b
-  p   new_pair = pair.reverse()
-      index_del = indexB + 1
-  p   num_array.insert(indexA, b).insert(indexB, a)
-    2.times do num_array.delete_at(index_del) end #times
-     p num_array
-     p num_array.flatten
-      new_pair = nil
-      i += 1
-    elsif b > a
-      i += 1
-    end
-  }
-    end     
-  end 
+        "i = #{i}"
+        indexA = i
+        indexB = i + 1
+        p pair = [num_array[indexA], num_array[indexB]]
+        a = pair[0]
+        b = pair[1]
+      if (a > b) && (i < i_length-1)
+          new_pair = pair.reverse
+          index_del = indexB + 1
+          num_array.insert(indexA, b).insert(indexB, a)
+          2.times { num_array.delete_at(index_del) }
+          num_array
+          p num_array.flatten
+          new_pair = nil
+          i += 1
+       elsif (a < b || a == b) && (i < i_length-1)
+          i += 1
+       end   
+      } 
+ end 
+end 
 
   module Enumerable
     def sorted?
